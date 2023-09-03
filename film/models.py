5b9d8ecc3afdc7800949e0d572485bc6b7c7e437
Joshua Olatunji <joshua@etentlabs.com>
2023-09-03 19:09:01 +0100

    film models

---
+++ film/models.py
@@ -0,0 +1,17 @@
+import uuid
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+
+class Film(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    release_date = models.DateTimeField(blank=True, null=True)
+
+
+class Comment(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    text = models.CharField(max_length=255, blank=True, null=True)
+    film = models.ForeignKey(
+        'Film', on_delete=models.SET_NULL, related_name="film_comments", null=True, blank=True
+    )
