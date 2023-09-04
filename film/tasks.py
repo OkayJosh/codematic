8f19cbd5a6c27849c94a61db5f939e7db9181247
Joshua Olatunji <joshua@etentlabs.com>
2023-09-04 01:06:16 +0100

    TASK: film tasks

---
+++ film/tasks.py
@@ -0,0 +1,30 @@
+import logging
+
+import requests
+from celery import shared_task
+
+from film.contants import SWAPI_FILMS, MAX_RETRY_ATTEMPTS, RETRY_DELAY
+from film.models import Film
+
+LOG = logging.getLogger(__name__)
+
+
+@shared_task(bind=True, max_retries=MAX_RETRY_ATTEMPTS, retry_backoff=RETRY_DELAY)
+def populate_with_swapi(*args, **kwargs):
+    try:
+        response = requests.get(SWAPI_FILMS)
+        LOG.info(f'{SWAPI_FILMS} called with response {response.status_code}')
+        response.raise_for_status()
+        if response.json()['count'] == Film.objects.count():
+            return
+        else:
+            data = response.json()['results']
+            for value in data:
+                films = Film.objects.filter(title=value['title'])
+                if not films.exists():
+                    Film.objects.create(title=value['title'], release_date=value['release_date'])
+
+            return
+    except requests.exceptions.Timeout as e:
+        LOG.exception(e)
+        raise self.retry()
