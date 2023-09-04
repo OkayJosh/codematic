24fc0bfa3dcb8a4ebba380c2b14e69d5376e0695
Joshua Olatunji <joshua@etentlabs.com>
2023-09-04 21:08:25 +0100

    Serializer: enforce comment_count

--- film/serializers.py
+++ film/serializers.py
@@ -4,6 +4,8 @@ from film.models import Film, Comment
 
 
 class FilmsSerializer(serializers.ModelSerializer):
+    comment_count = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Film
         fields = [
@@ -11,10 +13,13 @@ class FilmsSerializer(serializers.ModelSerializer):
             'comment_count',
         ]
 
+    def get_comment_count(self, obj):
+        return Comment.objects.filter(film__id=obj['id']).count()
+
 
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = [
-            'id', 'text', 'film__id'
+            'id', 'text', 'film'
         ]
