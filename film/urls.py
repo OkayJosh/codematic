d6488417f5540ada7856aee48393e2dc6b48c712
Joshua Olatunji <joshua@etentlabs.com>
2023-09-04 01:17:33 +0100

    VIEW: merge comment verbs to 1

--- film/urls.py
+++ film/urls.py
@@ -4,6 +4,5 @@ from film.views import FilmsViewSet
 
 urlpatterns = [
     path("films/", FilmsViewSet.as_view({'get': 'films'}), name="films"),
-    path("comments/", FilmsViewSet.as_view({'get': 'comments'}), name="comments"),
-    path("comments/", FilmsViewSet.as_view({'post': 'comment_add'}), name="comments"),
+    path("comments/", FilmsViewSet.as_view({'get': 'comments', 'post': 'comment_add'}), name="comments"),
 ]
